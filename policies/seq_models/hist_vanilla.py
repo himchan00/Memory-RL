4f2eeb9f99c04b2f9fa3b860c24681fe5321569f
himchan00 <himchan00@gmail.com>
2025-07-30 14:01:29 +0900

    Add input layer norm to hist

--- policies/seq_models/hist_vanilla.py
+++ policies/seq_models/hist_vanilla.py
@@ -8,13 +8,14 @@ from torch import Tensor
 class Hist(nn.Module):
     name = "hist"
 
-    def __init__(self, input_size, hidden_size, n_layer, pdrop = 0.1, **kwargs):
+    def __init__(self, input_size, hidden_size, n_layer, pdrop = 0.1, layer_norm_epsilon = 1e-5, **kwargs):
         """
         hyp_emb: If true, use hyperbolic embedding for the history representation
         """
         super().__init__()
         assert input_size == hidden_size, "input size must be equal to hidden size for residual connection"
-        l_layer = [Block(hidden_size=hidden_size, dropout=pdrop) for _ in range(n_layer)]
+        l_layer = [Block(hidden_size=hidden_size, dropout=pdrop, layer_norm_epsilon=layer_norm_epsilon) for _ in range(n_layer)]
+        self.ln_in = nn.LayerNorm(input_size, layer_norm_epsilon)
         self.encoder = nn.Sequential(*l_layer)
         self.hidden_size = hidden_size
         self.num_layers = n_layer
@@ -28,7 +29,7 @@ class Hist(nn.Module):
         output: (T, B, hidden_size)
         h_n: (1, B, hidden_size)
         """
-        z = self.encoder(inputs)
+        z = self.encoder(self.ln_in(inputs))
         output = torch.cumsum(z, dim = 0) + h_0
 
         return output, output[-1].unsqueeze(0)
@@ -78,7 +79,6 @@ class MLP(nn.Module):
         self.c_fc = Conv1D(hidden_dim, input_dim)
         self.c_proj = Conv1D(input_dim, hidden_dim)
         self.act = NewGELUActivation()
-        print(dropout)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
