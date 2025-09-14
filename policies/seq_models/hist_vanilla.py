2af81a3647f5137f0392ef48b1efba069a126b24
himchan00 <himchan00@gmail.com>
2025-09-14 22:45:57 +0900

    print temb_mode for degugging

--- policies/seq_models/hist_vanilla.py
+++ policies/seq_models/hist_vanilla.py
@@ -18,6 +18,7 @@ class Hist(nn.Module):
 
         self.temb_mode = kwargs["temb_mode"]
         assert self.temb_mode in ["none", "add", "concat"]
+        print(f"Use Hist with temb_mode = {self.temb_mode}.")
         if self.temb_mode == "add":
             self.embed_timestep = SinePositionalEncoding(max_seq_length+1, hidden_size)
         elif self.temb_mode == "concat":
