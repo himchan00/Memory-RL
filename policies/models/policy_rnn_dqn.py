Use only memory permutation and not transition permutation in permutation training. Fix numerical issues.

policies/models/policy_rnn_dqn.py
@@ -100,8 +100,7 @@ class ModelFreeOffPolicy_DQN_RNN(nn.Module):
 
         if self.permutation_training:
             length, batch_size, _ = actions.shape
-            transition_perm, memory_perm = self.critic.head.seq_model.sample_permutation_indices(length-1, batch_size)
-            self.critic.head.transition_perm = self.critic_target.head.transition_perm = transition_perm
+            memory_perm = self.critic.head.seq_model.sample_permutation_indices(length-1, batch_size)
             self.critic.head.memory_perm = self.critic_target.head.memory_perm = memory_perm
         ### 1. Critic loss
         q_pred, q_target, d_loss = self.algo.critic_loss(
@@ -114,7 +113,6 @@ class ModelFreeOffPolicy_DQN_RNN(nn.Module):
             gamma=self.gamma,
         )
         if self.permutation_training:
-            self.critic.head.transition_perm = self.critic_target.head.transition_perm = None
             self.critic.head.memory_perm = self.critic_target.head.memory_perm = None
 
         # masked Bellman error: masks (T,B,1) ignore the invalid error

policies/models/policy_rnn_shared.py
@@ -134,8 +134,7 @@ class ModelFreeOffPolicy_Shared_RNN(nn.Module):
 
         if self.permutation_training:
             length, batch_size, _ = actions.shape
-            transition_perm, memory_perm = self.head.seq_model.sample_permutation_indices(length-1, batch_size)
-            self.head.transition_perm = self.head_target.transition_perm = transition_perm
+            memory_perm = self.head.seq_model.sample_permutation_indices(length-1, batch_size)
             self.head.memory_perm = self.head_target.memory_perm = memory_perm
 
         joint_embeds, d_forward = self.head.forward(actions=actions, rewards=rewards, observs=observs)
@@ -143,7 +142,6 @@ class ModelFreeOffPolicy_Shared_RNN(nn.Module):
             target_joint_embeds, _ = self.head_target.forward(actions=actions, rewards=rewards, observs=observs)
 
         if self.permutation_training:
-            self.head.transition_perm = self.head_target.transition_perm = None
             self.head.memory_perm = self.head_target.memory_perm = None
 
         ### 2. Critic loss

policies/models/recurrent_head.py
@@ -27,8 +27,7 @@ class RNN_head(nn.Module):
         self.permutation_training = config_seq.get("permutation_training", False)
         if self.permutation_training: # These will be set externally during training
             assert (self.obs_shortcut and config_seq.seq_model.name == "mate"), "Permutation training is only implemented for Mate with obs_shortcut=True"
-            # Permutation indices for transition and memory
-            self.transition_perm = None
+            # Permutation indices for memory
             self.memory_perm = None
             # In permutation training, target network and online network need to be distinguished
             self.is_target = None
@@ -101,15 +100,13 @@ class RNN_head(nn.Module):
                 h0 = ptu.zeros((1, output.shape[1], output.shape[2])).float()
                 output = torch.cat((h0, output), dim = 0) # add zero hidden state at t = 0
                 if self.permutation_training:
-                    trans_emb = output[1:] - output[:-1] # (T, B, dim)
-                    transition_idx = self.transition_perm.unsqueeze(-1).expand(-1, -1, self.hidden_dim)  # (T, B, dim)
-                    trans_emb_perm = trans_emb.gather(0, transition_idx)  # (T, B, dim)
-                    mem_emb = torch.cat((h0, trans_emb_perm.cumsum(dim = 0)), dim=0)  # (T+1, B, dim)
+                    memory = output
                     memory_idx = self.memory_perm.unsqueeze(-1).expand(-1, -1, self.hidden_dim)  # (T+1, B, dim)
-                    mem_emb_perm = mem_emb.gather(0, memory_idx)  # (T+1, B, dim)
+                    memory_perm = memory.gather(0, memory_idx)  # (T+1, B, dim)
                     if self.is_target: # target joint emb: (s_{t+1}, m_{t} + trans(x_{t+1}))
-                        mem_emb_perm[1:] = mem_emb_perm[:-1].clone() + trans_emb
-                    output = mem_emb_perm
+                        trans_emb = (memory[1:] - memory[:-1]).clone()
+                        memory_perm[1:] = memory_perm[:-1].clone() + trans_emb
+                    output = memory_perm
             else:
                 output, _ = self.seq_model(inputs, initial_internal_state)
             return output

policies/seq_models/mate_vanilla.py
@@ -42,12 +42,10 @@ class Mate(nn.Module):
         """
 
         Returns:
-            transition_permutation: (episode_length, batch_size) long tensor
             memory_permutation: (episode_length+1, batch_size) long tensor
         """
-        transition_perm = ptu.rand(episode_length, batch_size).argsort(dim=0)  # (L, B)
         memory_perm = ptu.rand(episode_length+1, batch_size).argsort(dim=0)  # (L+1, B)
-        return transition_perm, memory_perm
+        return memory_perm
     
 
     def internal_state_to_hidden(self, internal_state):