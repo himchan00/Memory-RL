f99ba9070242083009faab7188b15515b1d82640
himchan00 <himchan00@gmail.com>
2026-01-27 15:29:37 +0900

    Add notebook for metaworld tsne plot

--- envs/metaworld.py
+++ envs/metaworld.py
@@ -29,9 +29,11 @@ class MLWrapper(gym.Wrapper):
         super().__init__(inner)
         
 
-    def _make_inner_env(self):
-        name = random.choice(list(self.classes.keys()))
-        env = self.classes[name](render_mode=self._render_mode_cfg)
+    def _make_inner_env(self, name: str=None):
+        if name is None:
+            name = random.choice(list(self.classes.keys()))
+        env = self.classes[name](render_mode=self._render_mode_cfg, camera_id=1)
+        self.name = name
         # Pick a random task for this env
         task = random.choice([t for t in self.tasks if t.env_name == name])
         env.set_task(task)
@@ -41,11 +43,17 @@ class MLWrapper(gym.Wrapper):
 
     def reset(self, **kwargs):
         # Re-create inner env each episode
-        self.env = self._make_inner_env()
+        if "name" in kwargs:
+            name = kwargs.pop("name")
+        else:
+            name = None
+        self.env = self._make_inner_env(name)
         return self.env.reset(**kwargs)
 
     def step(self, action):
-        return self.env.step(action)
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        info["name"] = self.name
+        return obs, reward, terminated, truncated, info
 
     def render(self):
         if self._render_mode_cfg is None:
